a13993726d5efd49232d4096c0f63ba4ae3f8241
lanbow93 <the.doctor1501@gmail.com>
2022-10-19 19:52:36 -0400

    minimum css done for positioning

--- projectFolder/js/app.js
+++ projectFolder/js/app.js
@@ -1,3 +1,4 @@
 // https://www.googleapis.com/books/v1/volumes?q={search+terms} << Find book by search terms
 // https://www.googleapis.com/books/v1/volumes/YJjdtQEACAAJ << Find book by ID
 
+
---
+++ test.js
@@ -0,0 +1,2 @@
+// https://www.googleapis.com/books/v1/volumes?q=Harry%20Potter%20and%20the%20Cursed%20Child
+
