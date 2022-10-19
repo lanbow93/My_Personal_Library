7ede35aeed525856d19b35052fb23201e192f334
lanbow93 <the.doctor1501@gmail.com>
2022-10-19 18:41:01 -0400

    Grid set out, bare min divs added in

--- projectFolder/js/app.js
+++ projectFolder/js/app.js
@@ -1,16 +1,3 @@
 // https://www.googleapis.com/books/v1/volumes?q={search+terms} << Find book by search terms
 // https://www.googleapis.com/books/v1/volumes/YJjdtQEACAAJ << Find book by ID
 
-
-function bookSearch() {
-    console.log("This function runs!")
-    const search = $("#search").val();
-    $("#results").innerhtml("");
-    console.log(search)
-
-    $.ajax({
-        url: "https://www.googleapis.com/books/v1/volumes?q=" + search
-    })
-}
-
-$("#button").on("click", bookSearch)
