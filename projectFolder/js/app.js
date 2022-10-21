1a0efecb3305378353e7678a47d098df4e8159ed
lanbow93 <the.doctor1501@gmail.com>
2022-10-21 17:27:08 -0400

    User input now calls for API properly

--- projectFolder/js/app.js
+++ projectFolder/js/app.js
@@ -1,8 +1,13 @@
 // https://www.googleapis.com/books/v1/volumes?q={search+terms} << Find book by search terms
 // https://www.googleapis.com/books/v1/volumes/YJjdtQEACAAJ << Find book by ID
 
+// Variables that will be used later
+let userInput;
+let bookData;
+
+//Areas I will need to grab or push data from frequently
 const $frequentLocations = {
-    serchBox: $("#searchBox"),
+    searchBox: $("#searchBox"),
     searchForm: $("#inputForm"),
     searchResults: $("#searchResults ul"),
     searchImage: $("#thumbnail img"),
@@ -11,10 +16,38 @@ const $frequentLocations = {
     shelfAddList: $("#addDropdown"),
     addForm: $("#addForm")
 }
+$frequentLocations.searchResults.append(`<li>Title: <span id='bookTitle'>Green Eggs and Ham</span><br>Author: Dr. Seuss</li>`)
+
+// What happens when book title is submitted
+function searchBook(event) {
+    event.preventDefault()
+    let userInput = $frequentLocations.searchBox.val()
+    $frequentLocations.searchBox.val("")
+
+    const Promise = $.ajax({
+        url: `https://www.googleapis.com/books/v1/volumes?q=${userInput}`
+    })
+
+    Promise.then(
+        (data) => {
+            bookData = data
+            
+        },
+        (error) => {
+            console.log(error)
+        }
+    )
+
+}
+
+// Click listener on the search box to look up book results
+$frequentLocations.searchForm.on("submit", searchBook);
 
 
 
- $frequentLocations.searchResults.append(`<li>Title: <span id='bookTitle'>Green Eggs and Ham</span><br>Author: Dr. Seuss</li>`)
+
+
+
 // $("ul").append(`<li>Title: <span id="bookTitle">Roots</span><br>Author: Alex Haley</li>`)
 // $("ul").append(`<li>Title: <span id="bookTitle">Roots</span><br>Author: Alex Haley</li>`)
 // $("ul").append(`<li>Title: <span id="bookTitle">Roots</span><br>Author: Alex Haley</li>`)
