48da570de66046f1eba522451e1108c440f0fbe4
lanbow93 <the.doctor1501@gmail.com>
2022-10-21 15:30:45 -0400

    DOM references made for elements

--- projectFolder/js/app.js
+++ projectFolder/js/app.js
@@ -2,8 +2,17 @@
 // https://www.googleapis.com/books/v1/volumes/YJjdtQEACAAJ << Find book by ID
 
 const $frequentLocations = {
+    serchBox: $("#searchBox"),
+    searchForm: $("#inputForm"),
     searchResults: $("#searchResults ul"),
+    searchImage: $("#thumbnail img"),
+    searchTitle: $("#titleLabel"),
+    searchDescription: $("#descriptionLabel"),
+    shelfAddList: $("#addDropdown"),
+    addForm: $("#addForm")
 }
+
+
 
  $frequentLocations.searchResults.append(`<li>Title: <span id='bookTitle'>Green Eggs and Ham</span><br>Author: Dr. Seuss</li>`)
 // $("ul").append(`<li>Title: <span id="bookTitle">Roots</span><br>Author: Alex Haley</li>`)
