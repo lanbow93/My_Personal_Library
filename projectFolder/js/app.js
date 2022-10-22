7e20ffd04aa09ec3e83dab29face371d3a5c1650
lanbow93 <the.doctor1501@gmail.com>
2022-10-22 08:45:38 -0400

    Changed array to obj to add key value pairs

--- projectFolder/js/app.js
+++ projectFolder/js/app.js
@@ -4,7 +4,7 @@
 // Variables that will be used later
 let userInput;
 let bookData;
-let bookArray = [];
+let bookList = {};
 
 //Areas I will need to grab or push data from frequently
 const $frequentLocations = {
@@ -17,25 +17,34 @@ const $frequentLocations = {
     shelfAddList: $("#addDropdown"),
     addForm: $("#addForm")
 }
-$frequentLocations.searchResults.append(`<li>Title: <span id='bookTitle'>Green Eggs and Ham</span><br>Author: Dr. Seuss</li>`)
+$frequentLocations.searchResults.append(`<li>Title: <span class='bookTitle'>Green Eggs and Ham</span><br>Author: Dr. Seuss</li>`)
 
 // Iterating though list of books and adding them to screen
 function listBooks(){
     // Clears out the current displayed results
     $frequentLocations.searchResults.empty();
     // Empties book array of their identifiers
-    bookArray = [];
+    bookList = {};
     // Initializes counter that will be used to give each element a unique ID
     counter = 0;
     for (let book of bookData.items){
-        $frequentLocations.searchResults.append(`<li id="a${counter}">Title: <span id='bookTitle'>${book.volumeInfo.title}</span><br>Author: ${book.volumeInfo.authors[0]}</li>`)
+        $frequentLocations.searchResults.append(`<li id="a${counter}">Title: <span class='bookTitle'>${book.volumeInfo.title}</span><br>Author: ${book.volumeInfo.authors[0]}</li>`)
         //Pushing a key value pair to later access if user wants more info
-        bookArray.push({
-            ["a"+counter]: book.id
-        })
+        bookList["a"+counter] = book.id
         counter++
     }
     
+}
+
+// Function to grab list item clicked, and use the ID to retireve the thumbnail and description
+function moreInfo(event) {
+
+    if (event.target.id === "") {
+        let test = event.currentTarget.id
+        console.log(bookList)
+    } else {
+        console.log(event.target.id)
+    }
 }
 
 // What happens when book title is submitted
@@ -52,6 +61,8 @@ function searchBook(event) {
         (data) => {
             bookData = data
             listBooks();
+            $("li").on("click", moreInfo)
+            console.log(data)
 
         },
         (error) => {
